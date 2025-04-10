d8483cdc036f83573cac0d96180a4c2b07cf9f9f
George Finch <georgefinch12@gmail.com>
2025-04-10 13:49:03 +0100

    basic database set up.

---
+++ VertoDeveloperTest/Models/DbInitializer.cs
@@ -0,0 +1,12 @@
+﻿namespace VertoDeveloperTest.Models
+{
+    public class DbInitializer
+    {
+        public static void Initialize(iOTAContext context)
+        {
+
+            context.Database.EnsureCreated();
+
+        }
+    }
+}
