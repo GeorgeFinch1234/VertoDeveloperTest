46eff03a78a7693ad35d5443f665e80965289692
George Finch <georgefinch12@gmail.com>
2025-04-10 14:20:37 +0100

    added in create funcality.

---
+++ VertoDeveloperTest/Controllers/CMS.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+using VertoDeveloperTest.Models;
+
+namespace VertoDeveloperTest.Controllers
+{
+    public class CMS : Controller
+    {
+        private readonly iOTAContext _context;
+        public CMS(iOTAContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create(IFormFile image, String title)
+        {
+
+            MemoryStream stream = new MemoryStream();
+            image.CopyTo(stream);
+            Byte[] data = stream.ToArray();
+            Carousel carousel = new Carousel();
+            carousel.Img = data;
+            carousel.Title = title;
+
+
+            _context.Add(carousel);
+            _context.SaveChanges();
+            return View("Index");
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
